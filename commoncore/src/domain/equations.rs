6e8f6c8d091cee13aa65e6ce3b5595690213ff52
Kanishk Gandhi <kanishk050296@gmail.com>
2022-03-16 16:20:58 -0700

    debug

--- commoncore/src/domain/equations.rs
+++ commoncore/src/domain/equations.rs
@@ -591,9 +591,6 @@ fn a_eval(t: &SizedTerm, i: usize, corrupt: f32) -> Option<(SizedTerm, String, S
                 if *op.to_string() == "+" {
                     println!("star worked");
                 }
-                if op == Add {
-                    println!("enum worked");
-                }
                 if *op == Add {
                     println!("star enum worked");
                 }
