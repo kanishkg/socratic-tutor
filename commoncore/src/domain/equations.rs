753579000a4d8afcd6447985628d89400366cc45
Kanishk Gandhi <kanishk050296@gmail.com>
2022-03-16 15:46:29 -0700

    debug

--- commoncore/src/domain/equations.rs
+++ commoncore/src/domain/equations.rs
@@ -632,7 +632,7 @@ fn a_eval(t: &SizedTerm, i: usize, corrupt: f32) -> Option<(SizedTerm, String, S
                 let mut answer = op.evaluate(n1, n2);
                 let mut is_corrupted:bool = false;
                 let mut corrupt_prob: f32 = rng.gen_range(0.0..1.0);
-                if corrupt_prob <  p * corrupt {
+                if corrupt_prob <  p {
                     let mut is_corrupted = true;
                     let mut answer: i32 = rng.gen_range(0..100);    
                 }
