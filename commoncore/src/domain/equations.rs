26b56895825388b2558657786cc0b2a1ad676e4e
Kanishk Gandhi <kanishk050296@gmail.com>
2022-03-16 16:37:43 -0700

    debug

--- commoncore/src/domain/equations.rs
+++ commoncore/src/domain/equations.rs
@@ -645,7 +645,7 @@ fn a_eval(t: &SizedTerm, i: usize, corrupt: f32) -> Option<(SizedTerm, String, S
 
                 if corrupt_prob <  p {
                     is_corrupted = true;
-                    answer: i32 = rng.gen_range(0..100);    
+                    let mut answer: i32 = rng.gen_range(0..100);    
                 }
                 return Some((SizedTerm::new(Number(answer), 1),
                              format!("eval {}, {}, {}", is_corrupted, i, t.to_string()),
